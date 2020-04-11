93ff2f787d4cdfddca6d8df0ca6c9a6a778103e6
Gustavo Lessa <guslessa@hotmail.com>
2020-04-11 16:34:07 -0300

    Inicial do meu projeto de Eventos

---
+++ ProAgil.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Models;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DataContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);    
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
+        }
+
+        // GET WeatherForecastController/2
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(result);    
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+           
+        }
+    }
+}
