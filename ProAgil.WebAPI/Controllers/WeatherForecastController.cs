4c998dde60508df0a04cdb6e911ee90635e98715
Gustavo Lessa <guslessa@hotmail.com>
2020-04-11 16:41:55 -0300

    teste commit

--- ProAgil.WebAPI/Controllers/WeatherForecastController.cs
+++ ProAgil.WebAPI/Controllers/WeatherForecastController.cs
@@ -70,7 +70,8 @@ namespace ProAgil.WebAPI.Controllers
             }
             catch (System.Exception)
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    "Banco de Dados Falhou");
             }
            
         }
